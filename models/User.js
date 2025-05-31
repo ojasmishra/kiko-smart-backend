bdc7bfda3894a40972340742f347f80f0edafb52
Ojas kumar mishra <Ojasmishra525@gmail.com>
2025-05-31 09:48:42 +0530

    seventh change

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: String,
+    name: String,
     email: { type: String, required: true, unique: true },
     password: String,
 
